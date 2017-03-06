a2a680b51b6d4026d0615ad973b31db4e026ede8
Andy Funk <acfunk@gmail.com>
2017-03-06 15:38:47 -0700

    Update get_actors.sql

--- queries/get_actors.sql
+++ queries/get_actors.sql
@@ -2,11 +2,9 @@ select
 	t.id
 	,t.tag
 	,ti.[index]
-from metadata_items mi
-join taggings ti
-	on mi.id = ti.metadata_item_id
+from taggings ti
 join tags t
 	on ti.tag_id = t.id
-where mi.id = 1
+where ti.metadata_item_id = 1
 	and t.tag_type = 6
 order by ti.[index];
